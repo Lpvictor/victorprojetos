2867b2d976820c12b28ba2d5c3982bc9cf36e808
Victor Luiz <125779677+Lpvictor@users.noreply.github.com>
2023-03-30 16:11:42 -0300

    Delete php/Victor directory

--- php/Victor/vicmat3.php
+++
@@ -1,9 +0,0 @@
-<form method="post">Número<input type="text" name="valor">
-<input type="submit" value="Enviar">
-</form>
-<?php
-function num($valor){
- echo floor($valor);
-}
-num($_POST['valor']);
-?>
--- php/Victor/vicmat4.php
+++
@@ -1,9 +0,0 @@
-<form method="post" name="valor">Número<input type="text" name="valor">
-    <input type="submit" value="Enviar">
-</form>
-<?php
-function num($valor){
-    echo sqrt($valor);
-}
-num ($_POST['valor']);
-?>
